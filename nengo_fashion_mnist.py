392374b55464cf92b5ff611b9474649c0d3b0635
Bartek <bartlomiej.pawel.borzyszkowski@intel.com>
2019-07-27 12:22:10 +0200

    fashion mnist example update

--- nengo_fashion_mnist.py
+++ nengo_fashion_mnist.py
@@ -14,7 +14,7 @@ from tensorflow import keras
 
 import nengo
 import nengo_dl
-import nengo_loihi
+# import nengo_loihi
 
 # keras uses the global random seeds, so we set those here to
 # ensure the example is reproducible
@@ -134,20 +134,20 @@ test_inputs = test_inputs.reshape((-1, net_input_shape))
 # in this case we'll just run for a single timestep).
 test_inputs = test_inputs[:, None, :]
 
-# with nengo_dl.Simulator(net, minibatch_size=minibatch_size) as sim:
-#     sim.step(data={input_node: test_inputs})
-#
-#
-# tensornode_output = sim.data[keras_p]
-#
-# for i in range(5):
-#     plt.figure()
-#     plt.imshow(test_images[test_inds][i], cmap="gray")
-#     plt.axis("off")
-#     plt.title("%s (%s)" % (
-#         class_names[test_labels[test_inds][i]],
-#         class_names[np.argmax(tensornode_output[i, 0])]));
-# plt.show()
-
-with nengo_loihi.Simulator(net, minibatch_size=minibatch_size) as sim:
+with nengo_dl.Simulator(net, minibatch_size=minibatch_size) as sim:
     sim.step(data={input_node: test_inputs})
+
+
+tensornode_output = sim.data[keras_p]
+
+for i in range(5):
+    plt.figure()
+    plt.imshow(test_images[test_inds][i], cmap="gray")
+    plt.axis("off")
+    plt.title("%s (%s)" % (
+        class_names[test_labels[test_inds][i]],
+        class_names[np.argmax(tensornode_output[i, 0])]));
+plt.show()
+
+# with nengo_loihi.Simulator(net, minibatch_size=minibatch_size) as sim:
+#     sim.step(data={input_node: test_inputs})
