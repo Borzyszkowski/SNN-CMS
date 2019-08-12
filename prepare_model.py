086f5c23b327deb9cd41c8233554d65275b31973
Bartek <bartlomiej.pawel.borzyszkowski@intel.com>
2019-08-12 11:29:10 +0200

    simulation files

--- prepare_model.py
+++ prepare_model.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 
 from Inputs_to_h5 import gen_h5
 import glob
+import tensorflow as tf
 
 # give paths to the dataset folder and json + h5 files
 data_path = './dataset'
@@ -42,21 +43,26 @@ def make_model(files, json_file, h5_file):
 
     x_train, x_val, y_train, y_val = train_test_split(features, target, test_size=0.33)
 
-    np.savez('x_test.npz', arr_0=x_val)
-    np.savez('y_test.npz', arr_0=y_val)
+    np.savez('x_test.npz', arr_0=features)
+    np.savez('y_test.npz', arr_0=target)
     print("npz dataset files created")
 
     # creating the model
     input_shape = x_train.shape[1]
-    dropoutRate = 0.25
+    print(input_shape)
+
+    # Add an op to initialize the variables.
+    init_op = tf.global_variables_initializer()
+
+    # Add ops to save and restore all the variables.
+
 
     inputArray = Input(shape=(input_shape,))
     x = Dense(64, activation='relu')(inputArray)
-    x = Dropout(dropoutRate)(x)
+    x = Dropout(0.25)(x)
     x = Dense(32, activation='relu')(x)
-    x = Dropout(dropoutRate)(x)
+    x = Dropout(0.35)(x)
     x = Dense(32, activation='relu')(x)
-    x = Dropout(dropoutRate)(x)
 
     output = Dense(5, activation='softmax')(x)
     model = Model(inputs=inputArray, outputs=output)
@@ -72,25 +78,30 @@ def make_model(files, json_file, h5_file):
     batch_size = 128
     n_epochs = 50
 
-    # training the model
-    history = model.fit(x_train, y_train, epochs=n_epochs, batch_size=batch_size, verbose=2,
-                        validation_data=(x_val, y_val),
-                        callbacks=[EarlyStopping(monitor='val_loss', patience=10, verbose=1),
-                                   ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1),
-                                   TerminateOnNaN()])
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        # training the model
+        history = model.fit(x_train, y_train, epochs=n_epochs, batch_size=batch_size, verbose=2,
+                            validation_data=(x_val, y_val),
+                            callbacks=[EarlyStopping(monitor='val_loss', patience=10, verbose=1),
+                                       ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1),
+                                       TerminateOnNaN()])
 
-    # visualizing the history
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.yscale('log')
-    plt.title('Training History')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['training', 'validation'], loc='upper right')
-    plt.savefig('ANN_training.png')
-    plt.show()
-    model.save_weights(h5_file)
-    print("h5 weight file saved")
+        # visualizing the history
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.yscale('log')
+        plt.title('Training History')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['training', 'validation'], loc='upper right')
+        plt.savefig('ANN_training.png')
+        plt.show()
+        model.save_weights(h5_file)
+        print("h5 weight file saved")
+        saver.save(sess, "./model_files/jet_file.ckpt")
+        print('model files saved: meta, index and data')
 
 
 if __name__ == "__main__":
